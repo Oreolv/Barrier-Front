322bc449333438b49305c81478b5a7bef1664f7e
“Oreolv” <455001000@qq.com>
2022-03-01 22:49:47 +0800

    feat: 增加tabBar

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,35 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ['pages/home/index', 'pages/serve/index', 'pages/mine/index'],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    navigationBarTitleText: '疫情社区管理防控',
+    navigationBarTextStyle: 'black',
+  },
+  tabBar: {
+    color: '#000',
+    selectedColor: '#fa2c19',
+    backgroundColor: '#fff',
+    borderStyle: 'white',
+    list: [
+      {
+        pagePath: 'pages/home/index',
+        text: '首页',
+        iconPath: './assets/icons/home.png',
+        selectedIconPath: './assets/icons/home-active.png',
+      },
+      {
+        pagePath: 'pages/serve/index',
+        text: '服务',
+        iconPath: './assets/icons/serve.png',
+        selectedIconPath: './assets/icons/serve-active.png',
+      },
+      {
+        pagePath: 'pages/mine/index',
+        text: '我的',
+        iconPath: './assets/icons/mine.png',
+        selectedIconPath: './assets/icons/mine-active.png',
+      },
+    ],
+  },
+});
---
+++ src/pages/serve/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <view class="index">
+    <div>{{ msg }}</div>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import './index.scss';
+const msg = ref('Hello world');
+</script>
