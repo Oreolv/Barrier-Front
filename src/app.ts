dd367acd3685a1e497acc68507bccfedf3504580
“Oreolv” <455001000@qq.com>
2022-03-03 22:01:27 +0800

    feat: 使用pinia

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue';
 import './app.scss';
 import '@nutui/nutui-taro/dist/style.css';
+import { createPinia } from 'pinia';
 import { Button } from '@nutui/nutui-taro';
 
+const pinia = createPinia();
 const App = createApp({
   onShow(options) {},
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 });
-App.use(Button);
+App.use(Button).use(pinia);
 
 export default App;
