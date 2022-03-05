b7367eda001949ddb0cc0124fab50bf32dcaac43
“Oreolv” <455001000@qq.com>
2022-03-05 20:20:14 +0800

    feat: 增加用户model

---
+++ src/model/user.ts
@@ -0,0 +1,31 @@
+/**
+ * @description 性别枚举
+ */
+export enum UserSex {
+  male,
+  female,
+}
+
+/**
+ * @description 通过微信获取的昵称和头像
+ */
+export interface UserInfoProp {
+  avatar: string;
+  nickName: string;
+}
+
+/**
+ * @description 通过数据库查询的用户个人信息
+ */
+export interface UserProfileProp {
+  id: number;
+  openid?: string;
+  uname?: string;
+  usex?: UserSex;
+  uphone?: number;
+  canme?: string;
+  vname?: string;
+  bnum?: string;
+  hnum?: string;
+  house?: number;
+}
