02937c4189969391f048849cd8b5d796d983c5b0
“Oreolv” <455001000@qq.com>
2022-03-08 22:56:51 +0800

    feat: 自定义Tabs

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,8 +1,15 @@
 <template>
   <SearchBar></SearchBar>
+  <nut-tabs v-model="tabValue" background="#FFF" title-gutter="15">
+    <nut-tabpane title="疫情资讯"> Tab 1 </nut-tabpane>
+    <nut-tabpane title="防疫知识"> Tab 2 </nut-tabpane>
+    <nut-tabpane title="社区公告"> Tab 3 </nut-tabpane>
+  </nut-tabs>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
 import SearchBar from '../../component/SearchBar';
 import './index.scss';
+const tabValue = ref(0);
 </script>
