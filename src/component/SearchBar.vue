2e5bc020ef324a33987313945630576fd56a94db
“Oreolv” <455001000@qq.com>
2022-03-10 13:22:03 +0800

    feat: 改写搜索栏逻辑并固定

--- src/component/SearchBar.vue
+++ src/component/SearchBar.vue
@@ -1,9 +1,13 @@
 <template>
-  <nut-searchbar :placeholder="placeholder">
-    <template v-slot:leftin>
-      <nut-icon size="14" name="search2"></nut-icon>
+  <nut-navbar :left-show="false">
+    <template #content>
+      <nut-searchbar :placeholder="placeholder">
+        <template v-slot:leftin>
+          <nut-icon size="14" name="search2"></nut-icon>
+        </template>
+      </nut-searchbar>
     </template>
-  </nut-searchbar>
+  </nut-navbar>
 </template>
 
 <script lang="ts" setup>
@@ -17,18 +21,29 @@ defineProps({
     type: String,
   },
 });
+const { statusBarHeight } = Taro.getSystemInfoSync();
 const { top, width, height, right } = Taro.getMenuButtonBoundingClientRect();
 const navTop = `${top - 9}px`; // 组件库含有9px的padding-top
 const navWidth = `${right - width}px`;
-const navHeight = `${height}px`;
+const inputHeight = `${height}px`;
+const navHeight = `${(top - statusBarHeight) * 2 + height + statusBarHeight}px`;
 </script>
 
 <style lang="scss">
+.nut-navbar {
+  position: fixed;
+  height: v-bind(navHeight);
+  top: 0;
+  z-index: 1;
+  width: 100%;
+}
 .nut-searchbar {
-  margin-top: v-bind(navTop);
+  position: fixed;
+  top: v-bind(navTop);
+  left: 0;
   width: v-bind(navWidth);
   .nut-searchbar__search-input {
-    height: v-bind(navHeight);
+    height: v-bind(inputHeight);
   }
 }
 </style>
