139dea2ef1ed219bef3b2dcf8182fbd973406fec
“Oreolv” <455001000@qq.com>
2022-03-24 21:52:57 +0800

    feat: 获取用户信息接口

--- src/api/model/usersModel.ts
+++ src/api/model/usersModel.ts
@@ -1,3 +1,19 @@
+import { UserSex, UserStatus } from '/@/enums/userEnum';
+
 export interface LoginParams {
   code: string;
+}
+
+export interface GetUserInfoResultModel {
+  uname: string;
+  usex: UserSex;
+  uphone: number;
+  cname: string;
+  vname: string;
+  bnum: string;
+  hnum: string;
+  hname: string;
+  idCard: string;
+  company: string;
+  foreignStatus: UserStatus;
 }
--- src/api/users.ts
+++ src/api/users.ts
@@ -1,9 +1,10 @@
 import { defHttp } from '/@/utils/axios';
-import { LoginParams } from './model/usersModel';
+import { LoginParams, LoginResultModel, GetUserInfoResultModel } from './model/usersModel';
 enum Api {
   Login = '/resident/login',
+  GetUserInfo = '/resident/getUserInfo',
 }
 
-export function login(params: LoginParams) {
-  return defHttp.post<void>({ url: Api.Login, params });
+export function getUserInfo() {
+  return defHttp.get<GetUserInfoResultModel>({ url: Api.GetUserInfo });
 }
