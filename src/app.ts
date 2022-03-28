75243f6349c4e122a8fad5d040f78c565e80f34f
“Oreolv” <455001000@qq.com>
2022-03-28 21:26:33 +0800

    feat: 引入iconfont

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue';
 import './app.scss';
+import '/@/assets/font/iconfont.css';
+import { createApp } from 'vue';
 import '@nutui/nutui-taro/dist/style.css';
 import { createPinia } from 'pinia';
 import {
