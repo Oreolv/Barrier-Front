1c03a4e5b7a247ddb3800625029c4914828e6a3d
“Oreolv” <455001000@qq.com>
2022-03-29 20:29:53 +0800

    feat: 个人资料添加登陆判断

--- src/pages/mine/data.ts
+++ src/pages/mine/data.ts
@@ -1,24 +1,29 @@
 import Taro from '@tarojs/taro';
+import { ShowToast } from '/@/hooks/useShowMessage';
 interface SettingItem {
   title: string;
   icon: string;
-  click?: () => void;
+  click: (loginStatus: boolean) => void;
 }
 export const SettingList: SettingItem[] = [
   {
     title: '个人资料',
     icon: 'wode-gerenziliao',
-    click: () => {
-      Taro.navigateTo({
-        url: '/pages/mine/children/user/index',
-        events: {},
-      });
+    click: (loginStatus) => {
+      if (loginStatus) {
+        Taro.navigateTo({
+          url: '/pages/mine/children/user/index',
+          events: {},
+        });
+      } else {
+        ShowToast.info('请先登陆');
+      }
     },
   },
   {
     title: '账户设置',
     icon: 'wode-bangdingguanxi',
-    click: () => {},
+    click: (loginStatus) => {},
   },
   {
     title: '分享应用',
