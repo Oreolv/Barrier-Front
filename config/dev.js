0d5841243fd2abdc211291b020995ef52ebdc278
“Oreolv” <455001000@qq.com>
2022-03-31 11:57:10 +0800

    feat: 添加dev压缩代码插件

--- config/dev.js
+++ config/dev.js
@@ -1,29 +1,11 @@
+const path = require('path');
+
 module.exports = {
   env: {
     NODE_ENV: '"development"',
   },
   defineConstants: {},
-  mini: {
-    webpackChain: (chain) => {
-      chain.merge({
-        plugin: {
-          install: {
-            plugin: require('terser-webpack-plugin'),
-            args: [
-              {
-                terserOptions: {
-                  compress: true, // 默认使用terser压缩
-                  // mangle: false,
-                  keep_classnames: true, // 不改变class名称
-                  keep_fnames: true, // 不改变函数名称
-                },
-              },
-            ],
-          },
-        },
-      });
-    },
-  },
+  mini: {},
   h5: {},
-  // plugins: [path.resolve(__dirname, 'plugins/minifyMainPackage.js')],
+  plugins: [path.resolve(__dirname, 'plugins/minifyMainPackage.js')],
 };
