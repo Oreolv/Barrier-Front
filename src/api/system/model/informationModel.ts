7356cb4e4b360b6eecae4a4eccca8200c844f7e6
“Oreolv” <455001000@qq.com>
2022-04-12 13:48:57 +0800

    feat: noticeModel

--- src/api/system/model/informationModel.ts
+++ src/api/system/model/informationModel.ts
@@ -1,6 +1,6 @@
-import { BasicFetchResult, BasicPageParams } from '/@/api/model/baseModel';
-
-export interface mediaInfo {
+import { BasicFetchResult, BasicPageParams, BasicTimeResult } from '/@/api/model/baseModel';
+import { UserHealthEnum as NoticeGradeEnum } from '/@/enums/userEnum';
+export interface MediaInfo {
   name: string;
   avatar: string;
   description: string;
@@ -12,11 +12,29 @@ export interface NewsItem {
   summary?: string;
   source?: string;
   cover?: string;
-  mediaInfo?: mediaInfo;
+  mediaInfo?: MediaInfo;
   sourceURL: string;
   publishTime: string;
+}
+
+export interface PublisherInfo {
+  username: string;
+  realName: string;
+  avatar: string;
+}
+
+export interface NoticeItem extends BasicTimeResult {
+  id: number;
+  content: string;
+  grade: NoticeGradeEnum;
+  publisher: number;
+  publisherInfo: PublisherInfo;
 }
 
 export type GetNewsListResultModel = BasicFetchResult<NewsItem>;
 
 export type GetNewsListParams = BasicPageParams;
+
+export type GetNoticeListResultModel = BasicFetchResult<NoticeItem>;
+
+export type GetNoticeListParams = BasicPageParams;
