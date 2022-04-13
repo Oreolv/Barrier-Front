8843de7951801e5b72e9690e33f10f20061c5cd3
“Oreolv” <455001000@qq.com>
2022-04-13 16:48:09 +0800

    feat: 服务基础页面

--- src/pages/serve/index.vue
+++ src/pages/serve/index.vue
@@ -1,11 +1,88 @@
 <template>
-  <view class="index">
-    <div>{{ msg }}</div>
-  </view>
+  <div class="top-img">
+    <img src="https://img.xinyu.ink//默认文件1649833707488.png" mode="widthFix" />
+  </div>
+  <div class="serve">
+    <div class="serve-header">日常防疫</div>
+    <div class="serve-content">
+      <nut-grid :column-num="4" :border="false" clickable>
+        <nut-grid-item icon="dongdong" text="来访申请">
+          <template #icon>
+            <nut-icon font-class-name="iconfont" class-prefix="icon" name="tongxunlu" />
+          </template>
+        </nut-grid-item>
+        <nut-grid-item icon="dongdong" text="行程报备">
+          <template #icon>
+            <nut-icon font-class-name="iconfont" class-prefix="icon" name="ziyuangongxiang" />
+          </template>
+        </nut-grid-item>
+        <nut-grid-item icon="dongdong" text="返乡报备">
+          <template #icon>
+            <nut-icon font-class-name="iconfont" class-prefix="icon" name="laifangshenpi" />
+          </template>
+        </nut-grid-item>
+        <nut-grid-item icon="dongdong" text="异常报备">
+          <template #icon>
+            <nut-icon font-class-name="iconfont" class-prefix="icon" name="tongzhi" />
+          </template>
+        </nut-grid-item>
+        <nut-grid-item icon="dongdong" text="意见反馈">
+          <template #icon>
+            <nut-icon font-class-name="iconfont" class-prefix="icon" name="youjian3" />
+          </template>
+        </nut-grid-item>
+      </nut-grid>
+    </div>
+  </div>
+  <div class="serve">
+    <div class="serve-header">紧急防疫</div>
+    <div class="serve-content">
+      <nut-grid :column-num="4" :border="false" clickable>
+        <nut-grid-item icon="dongdong" text="健康上报">
+          <template #icon>
+            <nut-icon font-class-name="iconfont" class-prefix="icon" name="renzheng" />
+          </template>
+        </nut-grid-item>
+        <nut-grid-item icon="dongdong" text="物资申请">
+          <template #icon>
+            <nut-icon font-class-name="iconfont" class-prefix="icon" name="che" />
+          </template>
+        </nut-grid-item>
+        <nut-grid-item icon="dongdong" text="事情代办">
+          <template #icon>
+            <nut-icon font-class-name="iconfont" class-prefix="icon" name="wenjian" />
+          </template>
+        </nut-grid-item>
+      </nut-grid>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
-import './index.scss';
-const msg = ref('Hello world');
-</script>
+<script setup></script>
+
+<style lang="scss">
+page {
+  box-sizing: border-box;
+  padding: 16px;
+}
+.top-img {
+  margin-bottom: 16px;
+  img {
+    width: 100%;
+    border-radius: 8px;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  }
+}
+.serve {
+  .serve-header {
+    font-weight: bold;
+    font-size: 18px;
+  }
+  .serve-content {
+    padding: 16px 0;
+  }
+}
+.iconfont {
+  font-size: 30px;
+}
+</style>
