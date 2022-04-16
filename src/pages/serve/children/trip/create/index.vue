b825590add417a1455cb67163b976c482cbd5476
“Oreolv” <455001000@qq.com>
2022-04-16 17:25:30 +0800

    feat: 行程报备框架

---
+++ src/pages/serve/children/trip/create/index.vue
@@ -0,0 +1,168 @@
+<template>
+  <nut-form :model-value="formValues">
+    <nut-form-item label="访客姓名" :rules="[{ required: true, message: '请填写访客姓名' }]">
+      <input
+        v-model="formValues.visitor"
+        class="nut-input-text"
+        placeholder="请输入姓名"
+        type="text"
+      />
+    </nut-form-item>
+    <nut-form-item label="访问时间" :rules="[{ required: true, message: '请填写访问时间' }]">
+      <input
+        v-model="rangeDate"
+        class="nut-input-text"
+        placeholder="请选择访问时间"
+        :disabled="true"
+        @click="state.showCalendar = true"
+      />
+    </nut-form-item>
+    <nut-form-item label="来自异地">
+      <nut-radiogroup direction="horizontal" v-model="formValues.foreign">
+        <nut-radio :label="0">否</nut-radio>
+        <nut-radio :label="1">是</nut-radio>
+      </nut-radiogroup>
+    </nut-form-item>
+    <nut-form-item label="来自何地" :rules="[{ required: true, message: '请填写访客家庭地址' }]">
+      <input
+        v-model="formValues.comeFrom"
+        class="nut-input-text"
+        placeholder="请输入访客的家庭住址"
+        type="text"
+      />
+    </nut-form-item>
+    <nut-form-item label="去往何地" :rules="[{ required: true, message: '请输入访客的去往地址' }]">
+      <input
+        v-model="formValues.goTo"
+        class="nut-input-text"
+        placeholder="请输入访客的去往地址"
+        type="text"
+      />
+    </nut-form-item>
+    <nut-form-item
+      label="访客的健康码与行程码截图"
+      :rules="[{ required: true, message: '请上传访客健康码行程码截图' }]"
+    >
+      <nut-uploader
+        :headers="state.uploadConfig.headers"
+        :url="state.uploadConfig.url"
+        multiple
+        @success="SuccessCallback"
+        @failure="ErrorCallback"
+        maximum="2"
+      ></nut-uploader>
+    </nut-form-item>
+  </nut-form>
+  <nut-calendar
+    v-model:visible="state.showCalendar"
+    type="range"
+    :show-title="false"
+    @choose="setChooseValue"
+    @close="state.showCalendar = false"
+  ></nut-calendar>
+  <div class="footer">
+    <nut-checkbox v-model="state.userPromise" label="本人承诺以上内容属实">
+      本人承诺以上内容属实
+    </nut-checkbox>
+    <nut-button type="primary" block @click="submitFormValues">提交申请</nut-button>
+  </div>
+</template>
+<script lang="ts" setup>
+import { reactive, computed } from 'vue';
+import { global } from '/@/utils/global';
+import { redirectTo } from '@tarojs/taro';
+import { useUserStore } from '/@/store/users';
+import { ShowToast } from '/@/hooks/useShowMessage';
+import { createVisitor } from '/@/api/serve/visitor';
+
+const userStore = useUserStore();
+
+const state = reactive({
+  showCalendar: false,
+  uploadConfig: {
+    url: `${global.apiUrl}/upload`,
+    headers: { Authorization: `Bearer ${userStore.getToken}` },
+  },
+  userPromise: true,
+});
+const formValues = reactive({
+  visitor: '',
+  foreign: 0,
+  comeFrom: '',
+  goTo: '',
+  startTime: '',
+  endTime: '',
+  healthCode: [] as string[],
+});
+
+const rangeDate = computed(() => {
+  return `${formValues.startTime}至${formValues.endTime}`;
+});
+
+const setChooseValue = (param) => {
+  formValues.startTime = param[0][3];
+  formValues.endTime = param[1][3];
+};
+
+const SuccessCallback = (res) => {
+  const data = JSON.parse(res.data.data);
+  if (data.code !== 0) {
+    ShowToast.error('上传失败');
+    return;
+  }
+  formValues.healthCode.push(data.result);
+};
+
+const ErrorCallback = () => {
+  ShowToast.error('上传失败');
+};
+
+const submitFormValues = async () => {
+  // 组件自带的实在是难用，还不如自己写
+  if (!formValues.visitor) {
+    ShowToast.info('请填写访客姓名');
+    return;
+  }
+  if (!formValues.startTime && !formValues.endTime) {
+    ShowToast.info('请填写访问时间');
+    return;
+  }
+  if (!formValues.comeFrom) {
+    ShowToast.info('请填写访客家庭住址');
+    return;
+  }
+  if (!formValues.goTo) {
+    ShowToast.info('请填写访客去往住址');
+    return;
+  }
+  if (formValues.healthCode.length < 2) {
+    ShowToast.info('请同时上传访客行程码和健康码');
+    return;
+  }
+  await createVisitor(formValues);
+  setTimeout(() => {
+    redirectTo({
+      url: '/pages/serve/children/visitor/index',
+    });
+  }, 1000);
+};
+</script>
+
+<style lang="scss">
+.calendar {
+  .nut-cell__title {
+    flex: 0 1 auto;
+    width: 100px;
+  }
+}
+.nut-cell__value {
+  color: #808080;
+}
+.footer {
+  padding: 16px;
+  .nut-button {
+    margin-top: 16px;
+    width: 100%;
+  }
+}
+</style>
--- src/pages/serve/children/trip/index.vue
+++ src/pages/serve/children/trip/index.vue
@@ -1,12 +1,146 @@
 <template>
-  <div></div>
+  <nut-tabs v-model="state.tabValue">
+    <template v-slot:titles>
+      <div
+        class="nut-tabs__titles-item"
+        @click="state.tabValue = '0'"
+        :class="{ active: state.tabValue == '0' }"
+      >
+        <span class="nut-tabs__titles-item__text">正在进行</span>
+      </div>
+      <div class="nut-tabs__titles-add" @click="createApply">
+        <nut-icon font-class-name="iconfont" class-prefix="icon" name="zengjia" />
+      </div>
+      <div
+        class="nut-tabs__titles-item"
+        @click="state.tabValue = '2'"
+        :class="{ active: state.tabValue == '2' }"
+      >
+        <span class="nut-tabs__titles-item__text">历史记录</span>
+        <!-- <span class="nut-tabs__titles-item__line"></span> -->
+      </div>
+    </template>
+    <nut-tabpane pane-key="0">
+      <nut-empty description="无数据" v-if="!state.currentList.length"></nut-empty>
+      <ApplyCardVue
+        v-for="item in state.currentList"
+        :key="item.id"
+        type="行程报备"
+        :status="item.status"
+        :title="`${item.visitor}行程报备`"
+      >
+        <template #content>
+          <div class="apply-content__item">
+            <div class="apply-content__item-name">开始时间</div>
+            <div class="apply-content__item-content">{{ item.startTime }}</div>
+          </div>
+          <div class="apply-content__item">
+            <div class="apply-content__item-name">结束时间</div>
+            <div class="apply-content__item-content">{{ item.endTime }}</div>
+          </div>
+          <div class="apply-content__item" v-if="item.status === ApplyStatusEnum.reject">
+            <div class="apply-content__item-name">拒绝理由</div>
+            <div class="apply-content__item-content">{{ item.description }}</div>
+          </div>
+        </template>
+      </ApplyCardVue>
+    </nut-tabpane>
+    <nut-tabpane pane-key="2">
+      <nut-empty description="无数据" v-if="!state.historyList.length"></nut-empty>
+      <ApplyCardVue
+        v-for="item in state.historyList"
+        :key="item.id"
+        type="行程报备"
+        :status="item.status"
+        :title="`${item.visitor}行程报备`"
+      >
+        <template #content>
+          <div class="apply-content__item">
+            <div class="apply-content__item-name">开始时间</div>
+            <div class="apply-content__item-content">{{ item.startTime }}</div>
+          </div>
+          <div class="apply-content__item">
+            <div class="apply-content__item-name">结束时间</div>
+            <div class="apply-content__item-content">{{ item.endTime }}</div>
+          </div>
+          <div class="apply-content__item" v-if="item.status === ApplyStatusEnum.reject">
+            <div class="apply-content__item-name">拒绝理由</div>
+            <div class="apply-content__item-content">{{ item.description }}</div>
+          </div>
+        </template>
+      </ApplyCardVue>
+    </nut-tabpane>
+  </nut-tabs>
 </template>
+<script lang="ts" setup>
+import { navigateTo } from '@tarojs/taro';
+import { reactive, onBeforeMount, ref } from 'vue';
+import ApplyCardVue from '/@/components/ApplyCard.vue';
+import { getVisitorList } from '/@/api/serve/visitor';
+import { GetVisitorListResultModel } from '/@/api/serve/visitor/model';
+import { getNodePositionInfo } from '/@/hooks/useGetSystemInfo';
+import { ApplyStatusEnum } from '/@/enums/serveEnums';
 
-<script lang="ts" setup></script>
+const state = reactive({
+  tabValue: '0',
+  currentList: [] as GetVisitorListResultModel,
+  historyList: [] as GetVisitorListResultModel,
+});
+
+const tabnineHeight = ref('80vh');
+
+const createApply = () => {
+  navigateTo({
+    url: '/pages/serve/children/visitor/create/index',
+  });
+};
+
+onBeforeMount(async () => {
+  const data = await getVisitorList();
+  const currentTime = new Date().getTime();
+  state.currentList = data.filter((i) => Date.parse(i.endTime.replace(/-/g, '/')) > currentTime);
+  state.historyList = data.filter((i) => Date.parse(i.endTime.replace(/-/g, '/')) <= currentTime);
+
+  tabnineHeight.value = `calc(100vh - ${
+    (await getNodePositionInfo('.nut-tabs__titles')).height
+  }px)`;
+});
+</script>
 
 <style lang="scss">
 page {
-  padding: 16px;
-  box-sizing: border-box;
+  background: #f5f5f5;
+}
+</style>
+
+<style lang="scss">
+.nut-tabs__titles {
+  padding: 0;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: white;
+  padding-bottom: constant(safe-area-inset-bottom);
+  padding-bottom: env(safe-area-inset-bottom);
+}
+.iconfont {
+  top: 16px;
+  color: #1a1a1a;
+  font-size: 36px;
+}
+.nut-tabs__titles-add {
+  display: flex;
+  width: 100px;
+  justify-content: center;
+}
+.nut-tabpane {
+  height: v-bind(tabnineHeight);
+  background: #f5f5f5;
+}
+.apply {
+  margin-top: 16px;
+}
+.apply:first-child {
+  margin-top: 0;
 }
 </style>
