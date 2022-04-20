bc5e24644feeb9f3235f9332bf92020e9635a90f
“Oreolv” <455001000@qq.com>
2022-04-20 17:07:17 +0800

    feat: 添加意见反馈类型

--- src/pages/serve/children/suggestion/create/data.ts
+++ src/pages/serve/children/suggestion/create/data.ts
@@ -10,4 +10,12 @@ export const formSchema = {
   content: '意见内容',
 };
 
-export const suggestionType = ['防疫相关', '人员投诉', '工作建议', '人员进出', '问题咨询', '其他'];
+export const suggestionType = [
+  '防疫相关',
+  '人员投诉',
+  '工作建议',
+  '人员进出',
+  '问题咨询',
+  '小程序优化',
+  '其他',
+];
