f86b3dae2d069b0f6d598200bdef5fbefb055444
“Oreolv” <455001000@qq.com>
2022-04-22 10:14:28 +0800

    feat: 封装头像信息头部组件

---
+++ src/components/AvatarHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="component">
+    <div class="avatar-header">
+      <div class="avatar-header__avatar">
+        <img :src="props.avatar" />
+      </div>
+      <div class="avatar-header__info">
+        <div class="avatar-info__name">{{ props.name }}</div>
+        <div class="avatar-info__description">
+          {{ props.description }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface AvatarHeaderProps {
+  avatar: string;
+  name: string;
+  description: string;
+}
+const props = withDefaults(defineProps<AvatarHeaderProps>(), {
+  avatar: '',
+  name: '',
+  description: '',
+});
+</script>
+
+<style lang="scss">
+.component {
+  .avatar-header {
+    display: flex;
+    align-items: center;
+    .avatar-header__avatar {
+      margin-right: 16px;
+      img {
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+      }
+    }
+    .avatar-header__info {
+      display: flex;
+      flex-direction: column;
+      font-size: 13px;
+      box-sizing: border-box;
+      justify-content: space-around;
+      .avatar-info__name {
+        margin-bottom: 4px;
+        color: #1a1a1a;
+        font-weight: bolder;
+      }
+      .avatar-info__description {
+        color: #666666;
+      }
+    }
+  }
+}
+</style>
--- src/pages/home/children/news/index.vue
+++ src/pages/home/children/news/index.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="media">
-    <div class="media-header">
-      <div class="media-header__avatar" v-if="params.mediaInfo.avatar">
-        <img :src="params.mediaInfo.avatar" alt="" />
-      </div>
-      <div class="media-header__info">
-        <div class="media-info__name">{{ params.mediaInfo.name }}</div>
-        <div class="media-info__description">
-          <!-- {{ params.mediaInfo.description || `${params.mediaInfo.name}报道` }} -->
-          {{ params.publishTime }}
-        </div>
-      </div>
-    </div>
+    <AvatarHeader
+      :avatar="params.mediaInfo.avatar || undefined"
+      :name="params.mediaInfo.name"
+      :description="params.publishTime"
+    />
     <div class="media-content">
       <div class="media-content__title">
         {{ params.title }}
@@ -24,8 +17,9 @@
 <script lang="ts" setup>
 import { getCurrentInstance } from '@tarojs/taro';
 import { NewsItem } from '/@/api/index/model/informationModel';
+import AvatarHeader from '/@/components/AvatarHeader.vue';
+
 const params: NewsItem = JSON.parse(decodeURIComponent(getCurrentInstance().router!.params.data!));
-// setNavigationBarTitle({ title: params.title });
 </script>
 
 <style lang="scss">
@@ -36,32 +30,6 @@ page {
 .media {
   display: flex;
   flex-direction: column;
-  .media-header {
-    display: flex;
-    .media-header__avatar {
-      margin-right: 16px;
-      img {
-        width: 36px;
-        height: 36px;
-        border-radius: 50%;
-        border: 1px #e5e5e5 solid;
-      }
-    }
-    .media-header__info {
-      display: flex;
-      flex-direction: column;
-      font-size: 13px;
-      box-sizing: border-box;
-      justify-content: space-around;
-      .media-info__name {
-        color: #1a1a1a;
-        font-weight: bolder;
-      }
-      .media-info__description {
-        color: #666666;
-      }
-    }
-  }
   .media-content {
     display: flex;
     flex-direction: column;
